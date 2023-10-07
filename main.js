3ea71064d95ed367137490f95eb8350905248c64
ivykibe <maleeivy@gmail.com>
2023-10-07 11:22:44 +0300

    Completed assignment

--- main.js
+++ main.js
@@ -3,9 +3,28 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const likeGlyph = document.querySelector('.like-glyph');
+const modal = document.querySelector('#modal'); // select the error modal element
+modal.classList.add('hidden'); // add the .hidden class to it
 
+const emptyHeart = document.querySelector('.empty-heart'); // select the empty heart element
 
+likeGlyph.addEventListener('click', () => {
+mimicServerCall()
+.then(() => {
+likeGlyph.innerHTML = '♥';
+likeGlyph.classList.add('activated-heart');
+})
+.catch (error => {
+ modalMessage.innerText = error
+ modal.classList.remove('hidden');
 
+setTimeout(() => {
+  modal.classList.add('hidden'); // add the .hidden class to hide the error modal after 3 seconds
+}, 3000);
+
+});
+});
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
